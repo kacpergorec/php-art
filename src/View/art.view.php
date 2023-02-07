9282628b23f01758498e37a0660a9cce71f330c4
kacpergorec <kacpergorec@gmail.com>
2023-02-07 18:19:58 +0100

    refactor: SVG Background done with rect not inline style to achieve full compatibility

--- src/View/art.view.php
+++ src/View/art.view.php
@@ -1,20 +1,20 @@
 <?php
 /** @var ArtCanvas $canvas */
+
 use KacperGorec\PhpArt\ArtCanvas;
+
 ?>
 
 <svg
         width="<?= $canvas->width ?>"
         height="<?= $canvas->height ?>"
-        style="background: <?= $canvas->palette->background ?>"
 >
-
     <defs>
         <?php foreach ($canvas->defs as $canvas->def): ?>
             <?= $canvas->def ?>
         <?php endforeach; ?>
     </defs>
-
+    <rect width="100%" height="100%" fill="<?= $canvas->palette->background ?>"/>
     <?php foreach ($canvas->shapes as $shape) {
         echo $shape;
     } ?>
